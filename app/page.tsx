5c49fe8ce5502d490c74435b5c6a1122345248d0
SuaDeDua <sokvai123hn@gmail.com>
2024-12-23 17:01:42 +0700

    ESlint & Prettier Setup (but something it not work)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 const Home = () => (
-  <h1 className="tex-3xl text-violet-700 font-black">New Nextjs application</h1>
+  <h1 className="text-3xl  text-violet-700 font-black">
+    New Nextjs application
+  </h1>
 );
 
 export default Home;
